645f90a7f8ee3ca2ca84b3ec9e2a2ffd977872d9
Sajid Shahriar <sajid.shahriar.iutcse@gmail.com>
2024-02-01 20:34:41 +0600

    Customized Admin Page

--- task_manager/tasks/admin.py
+++ task_manager/tasks/admin.py
@@ -4,5 +4,26 @@ from .models import Task, Image
 
 # Register your models here.
 
-admin.site.register(Task)
+
+class ImageInline(admin.TabularInline):
+    model = Image
+    extra = 3
+
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_by', 'created_at', 'due_date', 'priority', 'completed']
+
+
+    def get_queryset(self, request):
+        queryset = super(TaskAdmin, self).get_queryset(request)
+        return queryset.order_by('priority')
+
+
+    inlines = [ImageInline]
+
+
+
+
+admin.site.register(Task, TaskAdmin)
 admin.site.register(Image)
--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -8,23 +8,34 @@ User = get_user_model()
 
 
 class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     PRIORITY = {
-        (1, 'Low'),
+        (3, 'Low'),
         (2, 'Medium'),
-        (3, 'High'),
+        (1, 'High'),
     }
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     created_at = models.DateTimeField()
     due_date = models.DateTimeField()
     priority = models.IntegerField(blank=False, choices=PRIORITY)
-    isComplete = models.BooleanField(default=False)
+    is_complete = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
+
+    def created_by(self):
+        return self.user.username
+
+    def completed(self):
+        return self.is_complete
+
 
 
 class Image(models.Model):
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
     task_image = models.ImageField(upload_to="task_images")
+
+    def __str__(self):
+        return str(self.task.title)
 
